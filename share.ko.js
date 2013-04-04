ed0812058510c2bf5e5bdb5ddf1e1ea1d1f5291e
Ricardo Freitas <derfreitas@gmail.com>
2013-04-04 19:07:57 +0100

    broken commit

--- share.ko.js
+++ share.ko.js
@@ -6,22 +6,38 @@ ko:true
 
 (function(window, ko){
 
+    ko.binded = function( property, nestedProperty, target){
+
+        var setterGetter = function(value){
+            var propertyValue = typeof property === "function" ? property() : this[property]();
+
+            if (propertyValue){
+                return propertyValue[nestedProperty](value);
+            }
+        };
+
+        return ko.computed({
+            read: setterGetter,
+            write: setterGetter,
+            owner: target
+        })
+    };
+
 	//if the property is local the object sharejsdocument syncer ignores
 	ko.observable.fn.local = ko.computed.fn.local = function(){
-		console.log(arguments);
-		console.log(this);
 		this.isLocal = true;
 		return this;
 	};
 
 
-	ko.observable.fn.subscribeOnce = ko.computed.fn.subscribeOnce = function( callback, callbackTarget ){
-		var wrapper =  function(){
-			callback.apply(this, arguments);
-			sub.dispose();
-		};
-		var sub = this.subscribe.call( this, wrapper, callbackTarget );
-		return sub;
+	ko.observable.fn.subscribeOnce = ko.computed.fn.subscribeOnce = function( callback, callbackTarget, event ){
+		var sub = this.subscribe( function(){
+            var returnValue = callback.apply(this, arguments);
+            sub.dispose();
+            return returnValue;
+        }, callbackTarget, event );
+
+        return sub;
 	};
 
 
@@ -38,15 +54,41 @@ ko:true
 	var out =
 	window.sko = {};
 
+    var identityFunction = function(arg){return arg;};
 
-	var identityFunction = function(arg){return arg;};
+    out.subscribeWithHistory = function(property, callback, callbackTarget, previousValueHandler/*in case you want to modify the previousValue in same way first*/){
+        previousValueHandler = previousValueHandler || identityFunction;
+        var previousValue;
+        var subscriptionBeforeChange = property.subscribe(function(valueBeforeChange){
+            previousValue = extendSubscription.previousValueHandler(valueBeforeChange);
+        }, callbackTarget, "beforeChange");
+
+        /*why not use subscriptionOnce? because it does too many object deletions (with the dispose)*/
+        var subscription = property.subscribe(function(newValue){
+            return callback.call(this, newValue, previousValue);
+        }, callbackTarget);
+
+        var extendSubscription = Object.create(subscription);
+        extendSubscription.dispose = function(){
+            subscriptionBeforeChange.dispose();
+            return subscription.dispose();
+        };
+        extendSubscription.previousValueHandler = previousValueHandler;
+        return extendSubscription;
+    };
+
+    var subscribeWithHistory = function(){
+        return out.subscribeWithHistory.apply(this,arguments);
+    };
+
+
 
 	var koProperMapping = ko.propermap = function( koObj, plain, propCreator ){
 		//ko.mapping.fromJS( doc.get() , obj); buggy
 
 		propCreator = propCreator || identityFunction;
 
-		Object.keys( koObj ).forEach( function(key){
+		Object.keys( plain ).forEach( function(key){
 			var prop = koObj[key];
 			var propFromPlain =  propCreator( plain[key], key );
 
@@ -60,58 +102,179 @@ ko:true
 	};
 
 
+    var subscriptionsGroup = {
+        collection: null,
+        dispose: function(){
+            Object.keys(this.collection).forEach(function(sub){
+                sub.dispose();
+            });
+            this.collection = null;
+        },
+        disposeOf: function(subscriptionKey){
+            var sub = this[subscriptionKey];
+            if (sub) sub.dispose();
+            this[subscriptionKey] = undefined;
+        }
+    };
+
+    var extendSubscriptionGroup = function(properties){
+        var extendedObject = Object.create(subscriptionsGroup);
+        return _.extend(extendedObject, properties);
+    };
+
+    subscriptionsGroup = {
+        collection:[],
+        subscribe: function(){
+            this.collection.push( this.extendSubscription(objShare.on.apply(objShare,arguments)) );
+        },
+        extendSubscription: function(subscription){
+            var extension = Object.create(subscription);
+            extension.dispose = function(){
+                subscription.cb = function(){};//TODO: hack! this won't stop sharejs from calling it, albeit it will be calling an empty function
+            }
+            return extension;
+        }
+    };
+
+    var extendShareSubscriptionGroup = function(properties){
+        var extendedObject = Object.create(subscriptionsGroup);
+        return _.extend(extendedObject, properties);
+    }
+
+
+    var silentKVOProto =
+    out.silentKVOProto = {
+        _firedBySelf: false,
+        setter: function(koProperty, newValue){
+            this._firedBySelf = true;
+            koProperty( newValue );
+            this._firedBySelf = false;
+        },
+        caller: function(callback){
+            var silentKVO = this;
+            return function(){
+                if (!silentKVO._firedBySelf) return callback.apply(this, arguments);
+            }
+        },
+        //a subscription model that won't call the callback if the property was set with the silent(this) set
+        subscribe: function(koProperty, callback, callbackTarget){
+            return koProperty.subscribe( this.caller(callback), callbackTarget);
+        },
+        subscribeWithHistory: function(koProperty, callback, callbackTarget){
+            return subscribeWithHistory(koProperty, this.caller(callback), callbackTarget)
+        }
+    };
+
+
+
 
 	var objectSync =
 	out.objectSync = function( objKo, objShare, propertyCreator, initialSync ){
-		initialSync = initialSync || koProperMapping;
 
 		propertyCreator = propertyCreator || identityFunction;
 
 		//how to set the first sync?
 		//assume the server is authoritive, but if the server does not have the values
 		//then the local should be sent
+        initialSync = initialSync || koProperMapping;
+        koProperMapping.apply(this, arguments);
 
-		var kvoFiredByThisFunction = false;
-		Object.keys(objKo).forEach( function(key){
-			var prop = objKo[key];
-			if ( ko.isObservable( prop ) ){
-				var ko_prop = prop;
-				ko_prop.subscribe( function(newValue){
-					if ( !kvoFiredByThisFunction && !ko.isLocal( ko_prop ) ){
-						if ( typeof newValue === "object" ){
-							newValue = ko.toJS( newValue );
-						}
-						objShare.at(key).set( newValue );
-					}
-				});
-				prop = ko_prop();
-			}
+        var silentKVO = Object.create(silentKVOProto);
 
-			//deepness
-			if ( typeof prop === "object" && prop ){
-				//console.log(prop);
-				//objectSync( prop, objShare.at(key) );
-			}
-		});
+        var synchronizationState = {
+            childrenSyncs: extendSubscriptionGroup({
+                collection:{},
+                synchronize: function(propertyKey){
+                    var propertyValue = objKo[propertyKey]();
+                    if ( typeof propertyValue === "object" && propertyValue !== null ){
+                        this.disposeOf(propertyKey);
+                        this.collection[propertyKey] = objectSync( propertyValue, objShare.at(propertyKey) );
+                    }
+                }
+            }),
+            koSubscriptions: extendSubscriptionGroup({
+                collection:{},
+                subscribe: function(propertyKey){
+                    var property = objKo[propertyKey];
+                    if ( ko.isObservable( property ) ){
+                        this.disposeOf(propertyKey);
+                        this.collection[propertyKey] = silentKVO.subscribeWithHistory(
+                            property,
+                            this.propertySubscriptionHandler.bind(this, propertyKey)
+                        );
+                    }
+                },
+                subscribeToAll: function(){
+                    Object.keys(objKo).forEach( function(propertyKey){
+                        this.subscribe(propertyKey);
 
-		var setter = function( key, newValue ){
-			newValue = propertyCreator( newValue, key);
-			var prop = objKo[key];
-			if ( ko.isWriteableObservable( prop ) && !ko.isLocal( prop ) ){
-				kvoFiredByThisFunction = true;
-				prop( newValue );
-				kvoFiredByThisFunction = false;
-			}
-			else if ( !ko.isComputed( prop ) && !ko.isLocal( prop ) ){
-				objKo[key] = newValue;
-			}
-		};
+                        //deepness
+                        //this.synchronize(propertyKey);
+                    }.bind(this));
+                },
+                propertySubscriptionHandler: function(propertyKey, newValue, previousValue){
+                    var property = objKo[propertyKey];
+                    var childrenSyncs = synchronizationState.childrenSyncs;
+                    if ( !ko.isLocal( property ) ){
+                        childrenSyncs.disposeOf(propertyKey);
+                        if ( typeof newValue === "object" ){
+                            childrenSyncs.synchronize(propertyKey);
+                            newValue = ko.toJS( newValue );
+                        }
+                        objShare.at(propertyKey).set( newValue );
+                    }
+                }
+            }),
+            shareSubscriptions: extendSubscriptionGroup({
+                collection:[],
+                subscribe: function(){
+                    this.collection.push( this.extendSubscription(objShare.on.apply(objShare,arguments)) );
+                },
+                extendSubscription: function(subscription){
+                    var extension = Object.create(subscription);
+                    extension.dispose = function(){
+                        subscription.cb = function(){};//TODO: hack! this won't stop sharejs from calling it, albeit it will be calling an empty function
+                    }
+                    return extension;
+                }
+            }),
+            dispose: function(){
+                this.childrenSyncs.dispose();
+                this.koSubscriptions.dispose();
+                this.shareSubscriptions.dispose();
+            }
+        };
 
 
-		//this is crazy, sometimes the remoteop is called, other times it's the replace
-		//luckily it has been mutually exclusive
 
-		objShare.on( "remoteop", function(operations){
+        /*
+        * Knockout.js
+        * */
+
+        synchronizationState.koSubscriptions.subscribeToAll();
+
+
+        /*
+        * Share.js
+        * */
+        var shareSubscriptions = synchronizationState.shareSubscriptions;
+
+        //a change in document is set to the koObject with this setter
+        var shareJsSetter = function( key, property ){
+            var newProperty = propertyCreator( property, key);
+            var prop = objKo[key];
+            if ( ko.isWriteableObservable( prop ) && !ko.isLocal( prop ) ){
+                silentKVO.set(prop, newProperty);
+            }
+            else if ( !ko.isComputed( prop ) && !ko.isLocal( prop ) ){
+                objKo[key] = newProperty;
+            }
+        };
+
+        //this is crazy, sometimes the remoteop is called, other times it's the replace
+        //luckily it has been mutually exclusive
+
+        shareSubscriptions.subscribe("remoteop", function(operations){
 			console.log("remoteop");
 			console.log(arguments);
 			operations.forEach(function(operation){
@@ -124,25 +287,21 @@ ko:true
 
 				if ( "oi" in operation ){
 					var newValue = operation.oi;
-					setter( key, newValue );
+                    shareJsSetter( key, newValue );
 				}
 			});
 		});
 
 
 
-		objShare.on( "replace", function( key, oldVal, newVal){
+        shareSubscriptions.subscribe("replace", function( key, oldVal, newVal){
 			console.log("replace");
 			console.log(arguments);
-			setter( key, newVal );
+            shareJsSetter( key, newVal );
 		});
 
-		/*
-		objShare.on( "child op", function(){
-			console.log("child op");
-			console.log(arguments);
-		});
-		*/
+
+        return synchronizationState;
 	};
 
 
@@ -168,28 +327,31 @@ ko:true
 	};
 
 
-	var subscribeArray = function( observable, callback, callbackTarget ){
-		observable.subscribe( function(oldArray){
-			oldArray = oldArray.slice(0);
-			var sub = observable.subscribe( function(newArray){
-				callback.call( callbackTarget, newArray, oldArray );
-				sub.dispose();
-			} );
-		}, callbackTarget, "beforeChange");
+    var subscribeArray =
+    out.subscribeWithHistoryToArray = function( observable, callback, callbackTarget ){
+        return subscribeWithHistory(observable, callback, callbackTarget, function(arrayBeforeChange){
+            return arrayBeforeChange.slice(0)
+        });
 	};
 
-	var arrayKOInitialSync = function(arrayKO, arrayShare){
-		arrayShare.forEach(function(value, index){
-			arrayKO.setAt(index, arrayShare[index]);
+
+    //helpfull for duck friendliness and to avoid chained calls
+
+
+	var arrayKOInitialSync = function(arrayKO, arrayShare, itemCreator){
+		arrayShare.get().forEach(function(value, index){
+            value = itemCreator(value);
+			arrayKO.setAt(index, value);
 		});
 	};
 
 	var arraySync =
 	out.arraySync = function(arrayKO, arrayShare, itemCreator, docChange, strategy){
+
 		console.log("arraySync");
 		console.log(arguments);
 		strategy = strategy || arrayKOInitialSync;
-		strategy(arrayKO, arrayShare);
+		strategy(arrayKO, arrayShare, itemCreator);
 
 		itemCreator = itemCreator || identityFunction;
 		docChange = docChange || identityFunction;
@@ -310,6 +472,146 @@ ko:true
 			});
 		});
 	};
+
+    var isPropertySynchronizable = function(property){
+        return ko.isWriteableObservable(property) && !ko.isLocal(property);
+    };
+
+    var objectSynchronizableKeys = function(obj){
+        return Object.keys(obj).filter(function(prop){
+            return isPropertySynchronizable(prop);
+        });
+    };
+
+    var forEachSynchronizableProperty = function(obj, callback, callbackTarget){
+        var filteredKeys = objectSynchronizableKeys(obj);
+        callback = callback.bind(callbackTarget);
+        filteredKeys.forEach(function(propKey){
+            callback( obj[propKey], propKey);
+        });
+        return filteredKeys;
+    };
+
+    var isObservableArray = function(property){
+        return ko.observableArray.fn.isPrototypeOf(property);
+    };
+
+
+    out.propertyDocSync = function(property, doc){
+        var propSync;
+        if (isObservableArray(property)){
+            propSync = out.observableArrayDocSync(property);
+        }
+        else if (ko.isObservable(property)){
+            propSync = out.observableDocSync(property);
+        }
+        else if (typeof property === "object"){
+            propSync = out.objectDocSync = function(property, doc);
+        }
+        else{
+
+        }
+        return propSync;
+    };
+
+    var removeDoc = function(doc){
+        if (doc.remove){
+            doc.remove();
+        }
+        else{
+            doc.set(undefined);
+        }
+    };
+
+    out.valueDocSync = function(value, doc, parentSync, childKey){
+        var sync = {
+            parentSync: parentSync,
+            childKey: childKey,//key where you can find value in parent
+            value: value,
+            document: doc,
+            shareSubscriptions: null,
+            synchronize: function(){
+                var docValue = doc.get();
+                if (docValue !== undefined){
+                    parentSync.property[childKey] = docValue;
+                }
+                else{
+                    doc.set(value);
+                }
+            },
+            dispose: function(){
+                removeDoc(doc);
+            },
+            init: function(){
+            }
+        };
+    };
+
+    out.objectDocSync = function(object, doc){
+        var sync = {
+            property: object,
+            document: doc,
+            shareSubscriptions: null,
+            childSyncs: extendSubscriptionGroup({
+                collection: {},
+                syncNew: function(){
+                    Object.keys(object).forEach(function(property, propKey){
+                        var propSync;
+                        if ( this.collection[propKey].property !== property){
+                            collection[propKey] = out.propertyDocSyn(property, doc.at(propKey));
+                        }
+                        else{
+                            console.log("property with key:"+ propKey +" is already synced");
+                        }
+                    }, this);
+                },
+                reSync: function(){
+                    //deletes old syncs
+                    //syncs new properties
+                    this.syncNew();
+                }
+            }),
+            dispose: function(){
+                childSyncs.dispose();
+                doc.
+            },
+            synchronize: function(){
+                this.childSyncs.reSync();
+            },
+            init: function(){
+            }
+        };
+    };
+
+    out.observableDocSync = function(observable, doc ){
+
+        var sync = {
+            value: observable,
+            document: doc,
+            koSubscription: null,
+            shareSubscriptions: null,
+            subscriptionFunction: function(){
+                this.synchronize();
+            },
+            syncOfObservableValue: null,
+            synchronize: function(){
+                var observableValue = observable();
+                if (this.syncOfObservableValue) this.syncOfObservableValue.dispose();
+                this.syncOfObservableValue = out.propertyDocSync(observableValue);
+            },
+            init: function(){
+                this.koSubscription = observable.subscribe(this.subscriptionFunction);
+            }
+        };
+
+        sync.init();
+
+        return sync;
+    };
+
+    out.observableArrayDocSync = function(observableArray, doc){
+
+    };
 
 })(window, ko);
 
